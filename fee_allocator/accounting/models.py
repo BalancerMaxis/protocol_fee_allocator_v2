30e3506830febc84ecc11ba127e640d20209ad27
jalbrekt85 <jcalbrecht85@gmail.com>
2024-09-18 16:17:02 -0500

    add docs for models.py

--- fee_allocator/accounting/models.py
+++ fee_allocator/accounting/models.py
@@ -8,6 +8,11 @@ InputFees = Dict[NewType("CorePoolChainName", str), NewType("FeesCollected", int
 
 
 class GlobalFeeConfig(BaseModel):
+    """
+    Represents the global fee configuration for the fee allocation process.
+    Models the data sourced from the FEE_CONSTANTS_URL endpoint.
+    """
+
     min_aura_incentive: int
     min_existing_aura_incentive: int
     min_vote_incentive_amount: int
@@ -17,6 +22,11 @@ class GlobalFeeConfig(BaseModel):
 
 
 class RerouteConfig(BaseModel):
+    """
+    Represents the configuration for rerouting fees in the fee allocation process.
+    Models the data sourced from the REROUTE_CONFIG_URL endpoint.
+    """
+
     mainnet: Dict
 
     def model_post_init(self, __context):
